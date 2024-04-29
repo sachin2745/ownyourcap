0954e9c5a8736bc52916fd343e41f285fb968aff
sachin2745 <sachin.official648@gmail.com>
2024-04-29 18:18:16 +0530

    problem solved in myShop

--- frontend/src/app/(main)/MyShop/page.jsx
+++ frontend/src/app/(main)/MyShop/page.jsx
@@ -25,7 +25,7 @@ const page = () => {
         fetchPostData();
     }, []);
 
-    
+
 
 
     return (
@@ -34,34 +34,47 @@ const page = () => {
                 <h1 className='font-bold text-3xl'>My Shop</h1>
                 <h3 className='text-1xl'>Home &rsaquo; My Shop</h3>
             </div>
-            <div className="container px-5 py-24 mx-auto">
-                {
-                    postArray.map((post) =>
-                        <div key={post._id} 
-                         className=" mt-6 w-80  bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300  j p-4 gap-4 rounded-lg shadow-md">
+            <div className="grid grid-cols-4 gap-4">
+            {
+                postArray.map((post) => {
+                    return(
                         
-                            <div className="lg:w-80 md:w-1/4  w-full">
-                                <a className="block relative h-48 rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="object-cover object-center w-72 h-full block"
-                                        src={post.image}
-                                    />
-                                </a>
-                                <div className="mt-4">
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                    {post.name}
-                                    </h2>
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        {post.description}
-                                    </h3>
-                                    <p className="mt-1">Rs.{post.price}</p>
-                                    <button type="submit" className="flex ml-36 text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-primary rounded">Add to Cart</button>
+                        <div>
+                            <div className="container px-5 py-24 mx-auto">
+                                <div
+                                    className=" mt-6 w-80  bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300  j p-4 gap-4 rounded-lg shadow-md">
+        
+                                    <div className="lg:w-80 md:w-1/4  w-full">
+                                        <a className="block relative h-48 rounded overflow-hidden">
+                                            <img
+                                                alt="ecommerce"
+                                                className="object-cover object-center w-72 h-full block"
+                                                src={post.image}
+                                            />
+                                        </a>
+                                        <div className="mt-4">
+                                            <h2 className="text-gray-900 title-font text-lg font-medium">
+                                                {post.name}
+                                            </h2>
+                                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                                                {post.description}
+                                            </h3>
+                                            <p className="mt-1">{post.price}</p>
+                                            <button type="submit" className="flex ml-36 text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-primary rounded">Add to Cart</button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>                           
-                        </div>)
-                }
-            </div>
+        
+                            </div>
+                        </div>
+        
+        
+                  
+                    )
+                } )
+            }
+             </div>
+
         </section>
 
     )
